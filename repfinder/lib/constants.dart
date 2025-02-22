a38269bc414b2542246f406730bd34c7a39879f7
trev-org04 <58343522+trev-org04@users.noreply.github.com>
2025-02-22 12:04:00 -0500

    Flutter init commit

---
+++ repfinder/lib/constants.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color lavender = Color(0xFFCBB0E8);
+  static const Color periwinkle = Color(0xFF8C9CD9);
+  static const Color navy = Color(0xFF40508C);
+  static const Color black = Color(0xFF211E12);
+  static const Color white = Color(0xFFEFECE1);
+}
