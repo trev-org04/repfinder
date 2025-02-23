796de43957a9ca420f4c85c40ae95ed09875e249
trev-org04 <58343522+trev-org04@users.noreply.github.com>
2025-02-23 08:22:15 -0500

    Update machine cards

--- repfinder/lib/home/home.dart
+++ repfinder/lib/home/home.dart
@@ -9,6 +9,18 @@ void main() {
   runApp(HomePage());
 }
 
+class Machine {
+  final String name;
+  final String muscleGroup;
+  final String status;
+
+  Machine({
+    required this.name,
+    required this.muscleGroup,
+    required this.status,
+  });
+}
+
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
 
@@ -19,20 +31,64 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   String firstName = "Loading...";
   int _selectedIndex = 0;
+  List<Machine> machines = [];
+  List<Machine> selectedMachines = [];
+  List<String> muscleGroups = [
+    'Abs',
+    'Biceps',
+    'Back',
+    'Chest',
+    'Legs',
+    'Shoulders',
+    'Triceps',
+    'Cardio',
+  ];
 
   @override
   void initState() {
     super.initState();
     fetchUserName();
+    fetchMachines();
   }
 
-  Future<void> fetchUserName() async {
+  void fetchUserName() async {
     final user = supabase.auth.currentUser;
     if (user != null && user.userMetadata != null) {
       setState(() {
         firstName = user.userMetadata!['first_name'] ?? "User";
       });
     }
+  }
+
+  Future<void> fetchMachines() async {
+    final response = await supabase.from('machines').select();
+    List<Machine> machineData = [];
+    for (var row in response) {
+      machineData.add(
+        Machine(
+          name: row['name'] as String,
+          muscleGroup: row['muscle_group'] as String,
+          status: row['status'] as String,
+        ),
+      );
+    }
+    setState(() {
+      machines = machineData;
+    });
+    fetchSelectedMachines();
+  }
+
+  void fetchSelectedMachines() {
+    List<Machine> machineData = [];
+    for (var machine in machines) {
+      if (machine.muscleGroup.toLowerCase() ==
+          muscleGroups[_selectedIndex].toLowerCase()) {
+        machineData.add(machine);
+      }
+    }
+    setState(() {
+      selectedMachines = machineData;
+    });
   }
 
   // called when flutter needs to rebuild ui
@@ -68,6 +124,7 @@ class _HomePageState extends State<HomePage> {
                 borderRadius: BorderRadius.circular(20),
                 color: AppColors.offblack,
               ),
+              // Capacity Indicator
               child: Padding(
                 padding: const EdgeInsets.all(10),
                 child: Row(
@@ -133,6 +190,7 @@ class _HomePageState extends State<HomePage> {
                             style: GoogleFonts.inter(
                               fontSize: 15,
                               fontWeight: FontWeight.w400,
+                              color: AppColors.white.withOpacity(0.5),
                             ),
                           ),
                         ],
@@ -142,22 +200,14 @@ class _HomePageState extends State<HomePage> {
                 ),
               ),
             ),
+            // Muscle Group SelectorS
             Padding(
               padding: const EdgeInsets.symmetric(vertical: 20.0),
               child: SingleChildScrollView(
                 scrollDirection:
                     Axis.horizontal, // Enables horizontal scrolling
                 child: Row(
-                  children: List.generate(7, (index) {
-                    List<String> muscleGroups = [
-                      'Abs',
-                      'Biceps',
-                      'Back',
-                      'Chest',
-                      'Legs',
-                      'Shoulders',
-                      'Triceps',
-                    ];
+                  children: List.generate(8, (index) {
                     return Padding(
                       padding: const EdgeInsets.only(right: 10.0),
                       child: GestureDetector(
@@ -165,6 +215,7 @@ class _HomePageState extends State<HomePage> {
                             () => {
                               setState(() {
                                 _selectedIndex = index;
+                                fetchSelectedMachines();
                               }),
                             },
                         child: Container(
@@ -179,7 +230,6 @@ class _HomePageState extends State<HomePage> {
                                 _selectedIndex == index
                                     ? AppColors.periwinkle
                                     : AppColors.offblack,
-                            // color: Colors.blueAccent,
                             borderRadius: BorderRadius.circular(10),
                           ),
                           child: Text(
@@ -203,33 +253,98 @@ class _HomePageState extends State<HomePage> {
                 ),
               ),
             ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.start,
-              children: [
-                Padding(
-                  padding: const EdgeInsets.only(top: 30.0),
-                  child: Container(
-                    width: MediaQuery.of(context).size.width * 0.42,
-                    height: 100,
+            // Machine Cards
+            Expanded(
+              child: GridView.builder(
+                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 2,
+                  crossAxisSpacing: 10,
+                  mainAxisSpacing: 10,
+                  childAspectRatio: 1,
+                ),
+                itemCount: selectedMachines.length,
+                itemBuilder: (context, index) {
+                  final machine = selectedMachines[index];
+                  return Container(
+                    width: MediaQuery.of(context).size.width * 0.5 - 25,
                     decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(20),
+                      borderRadius: BorderRadius.circular(15),
                       color: AppColors.offblack,
                     ),
-                  ),
-                ),
-                SizedBox(width:30),
-                Padding(
-                  padding: const EdgeInsets.only(top: 30.0),
-                  child: Container(
-                    width: MediaQuery.of(context).size.width * 0.42,
-                    height: 100,
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(20),
-                      color: AppColors.offblack,
+                    child: Padding(
+                      padding: const EdgeInsets.all(10.0),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Text(
+                                machine.name,
+                                style: TextStyle(
+                                  fontFamily: 'Polymath',
+                                  fontSize: 16,
+                                  fontVariations: [FontVariation('wght', 600)],
+                                  color: AppColors.white,
+                                  letterSpacing: -0.5,
+                                ),
+                              ),
+                            ],
+                          ),
+                          Image.asset(
+                            'assets/images/${machine.name.toLowerCase().replaceAll(' ', '_')}.png',
+                            width: 100,
+                            fit: BoxFit.contain,
+                          ),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            crossAxisAlignment: CrossAxisAlignment.center,
+                            children: [
+                              Row(
+                                children: [
+                                  Icon(
+                                    CupertinoIcons.person,
+                                    size: 15,
+                                    color: AppColors.white.withOpacity(0.5),
+                                  ),
+                                  Padding(
+                                    padding: const EdgeInsets.only(left: 5.0),
+                                    child: Text(
+                                      '5',
+                                      style: GoogleFonts.inter(
+                                        fontSize: 15,
+                                        color: AppColors.white.withOpacity(0.5),
+                                      ),
+                                    ),
+                                  ),
+                                ],
+                              ),
+                              ElevatedButton(
+                                style: ButtonStyle(
+                                  padding: WidgetStateProperty.all(
+                                    EdgeInsets.zero,
+                                  ),
+                                  minimumSize: WidgetStateProperty.all(
+                                    Size(30, 30),
+                                  ),
+                                  backgroundColor: WidgetStateProperty.all(
+                                    AppColors.periwinkle,
+                                  ),
+                                  shape: WidgetStateProperty.all(
+                                    CircleBorder(),
+                                  ),
+                                ),
+                                onPressed: () {},
+                                child: Icon(Icons.add, size: 15),
+                              ),
+                            ],
+                          ),
+                        ],
+                      ),
                     ),
-                  ),
-                ),
-              ],
+                  );
+                },
+              ),
             ),
           ],
         ),
